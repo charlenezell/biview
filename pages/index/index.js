fddd4c80ab86a3ff3b4ca74ddc97bc4be500e5b2
charlenezell <charlenezell@gmail.com>
2017-05-12 17:47:12 +0800

    bk aoqi demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,46 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  bindKeyInput: function (e) {
+    this.setData({
+      keyword:e.detail.value
+    });
+    if (e.detail.value.trim()) {
+      let rst = app.filterArrayByKeywords(this.data.totleRankList, "elfName", e.detail.value)
+      if (rst.length > 0) {
+        this.setData({
+          filteredTotleRankList: rst
+        })
+        // this.setData({
+        // toView:"jl_"+rst[0].elfName
+        // });
+      } else {
+        this.setData({
+          filteredTotleRankList: []
+        });
+      }
+    } else {
+      this.setData({
+        filteredTotleRankList: this.data.totleRankList
+      })
+    }
+
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+    app.getJLTotalRank().then(data => {
+      let addRank=data.map((v,k)=>{
+        return Object.assign({},v,{
+          rank:k+1
+        });
+      })
+      this.setData({
+        totleRankList:addRank ,
+        filteredTotleRankList: addRank
       })
     })
   }
