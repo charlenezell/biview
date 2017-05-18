fa3cc705cec102220797a5a218ef7ff00de15020
charlenezell <charlenezell@gmail.com>
2017-05-18 23:09:10 +0800

    refact with weui

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,31 @@
 var app = getApp();
 Page({
   data: {
-
+    inputShowed: false,
+    inputVal: ""
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false,
+      filteredTotleRankList: this.data.totleRankList
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: "",
+      filteredTotleRankList: this.data.totleRankList
+    });
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
   bindKeyInput: function (e) {
     this.setData({
-      keyword:e.detail.value
+      keyword: e.detail.value,
+      inputVal: e.detail.value
     });
     if (e.detail.value.trim()) {
       let rst = app.filterArrayByKeywords(this.data.totleRankList, "elfName", e.detail.value)
@@ -33,13 +53,13 @@ Page({
   onLoad: function () {
     //调用应用实例的方法获取全局数据
     app.getJLTotalRank().then(data => {
-      let addRank=data.map((v,k)=>{
-        return Object.assign({},v,{
-          rank:k+1
+      let addRank = data.map((v, k) => {
+        return Object.assign({}, v, {
+          rank: k + 1
         });
       })
       this.setData({
-        totleRankList:addRank ,
+        totleRankList: addRank,
         filteredTotleRankList: addRank
       })
     })
